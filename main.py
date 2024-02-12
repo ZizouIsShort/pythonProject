e9bf66589ded82e115d9b445007dec8962a12dda
ziyansolkar2005 <145798661+ziyansolkar2005@users.noreply.github.com>
2024-02-12 12:57:07 +0530

    using fetch requests in js and using post method in flask
    Did this to take in the users email and print it in the terminal

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from flask import Flask,render_template
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def login():
-    return render_template('loginpage.html')
+def index():
+    return render_template("loginpage.html")
 
-if __name__ == '__main__':
+@app.route("/process_input", methods=["POST"])
+def process_input():
+    try:
+        data = request.get_json()
+        user_input = data["input"]
+        if not user_input:
+            raise ValueError("Missing 'input' data in request body")
+
+        print("Users email is :", user_input)
+        response = {"message": "Input received successfully!"}
+        return jsonify(response)
+
+    except Exception as e:
+        print(f"Error processing input: {e}")
+        return jsonify({"error": str(e)}), 400
+
+if __name__ == "__main__":
     app.run(debug=True)
